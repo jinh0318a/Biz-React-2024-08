d45f39d8f2d7d71c5fa25a4def2347f1ad323022
jinh0318a <jinh0318a@naver.com>
2024-08-07 14:10:57 +0900

    todoList

--- react-todo-001/src/comps/TodoInput.js
+++ react-todo-001/src/comps/TodoInput.js
@@ -9,12 +9,22 @@ import { getTodo } from "../modules/TodoUtils";
  */
 const TodoInput = ({ todoList, setTodoList }) => {
   const [todo, setTodo] = useState("");
+
+  const onClickHandler = () => {
+    const todoJSON = getTodo(todo);
+    /**
+     * 기존의 todoList 배열을 펼쳐서(Spread) 깊은 복사본을 만들고
+     * 생성된 복사본 끝에 todoJSON 의 데이터를 추가하여 배열로 생성
+     * 그리고, 그 생성된 배열을 기존의 todoList와 교체
+     * setTodoList()는 기존의 todoList 배열을 새로운 배열로 교체
+     */
+    setTodoList([...todoList, todoJSON]);
+  };
+
   return (
     <div className="todoInput">
       <input onChange={(e) => setTodo(e.target.value)} placeholder="TODO" />
-      <button onClick={() => setTodoList([...todoList, getTodo(todo)])}>
-        추가
-      </button>
+      <button onClick={onClickHandler}>추가</button>
     </div>
   );
 };
